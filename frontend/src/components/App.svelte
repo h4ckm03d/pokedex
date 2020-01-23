ad4db6cefd08eda55cced7eb82dbe2cb7a345dbc
Moch Lutfi <kapten.lufi@gmail.com>
2020-01-23 14:00:25 +0700

    Added input search

--- frontend/src/components/App.svelte
+++ frontend/src/components/App.svelte
@@ -2,13 +2,25 @@
   import { onMount } from "svelte";
   import CardInfo from "./CardInfo.svelte";
   let cards = [];
+  let query = "";
+  let page;
 
   onMount(async () => {
-    const res = await fetch(`https://api.pokemontcg.io/v1/cards?pageSize=10`);
-    console.log(res);
+    fetchData();
+  });
+
+  async function fetchData() {
+    const res = await fetch(
+      `https://api.pokemontcg.io/v1/cards?pageSize=10&name=${query}`
+    );
     const payload = await res.json();
     cards = payload.cards;
-  });
+  }
+
+  function searchClick() {
+    console.log("klicked", query)
+    fetchData();
+  }
 </script>
 
 <style>
@@ -18,22 +30,36 @@
    grid-gap: 20px; */
   }
 </style>
+
 <section class="hero">
   <div class="hero-body">
     <div class="container">
-      <h1 class="title">
-        Pokedex
-      </h1>
-      <h2 class="subtitle">
-        Pokemon TCG
-      </h2>
+      <h1 class="title">Pokedex</h1>
+      <h2 class="subtitle">Pokemon TCG</h2>
     </div>
   </div>
 </section>
 <hr />
 <div class="container">
+  <div class="field has-addons">
+    <p class="control">
+      <input
+        id="search-input-text"
+        class="input"
+        bind:value={query}
+        placeholder="Filter by name, types" />
+    </p>
+    <p class="control">
+    <button class="button" on:click={searchClick}>
+      <span class="icon icon-input-search">
+        <i class="fas fa-search" />
+      </span>
+      <span>Search</span>
+    </button>
+  </p>
+  </div>
   {#each cards as card}
-    <CardInfo {card} />
+    <CardInfo card={card} />
     <hr />
   {:else}
     <!-- this block renders when cards.length === 0 -->
